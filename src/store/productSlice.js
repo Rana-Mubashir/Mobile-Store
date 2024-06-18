5e066706a0f0ea5c8f8e004e6b762b35da4386fb
Faraz Ahmad <farazahmad31048@gmail.com>
2024-06-18 10:08:42 +0500

    initial commit

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,16 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
     productToFind:'',
 }
+
 const productSlice = createSlice({
     name: 'Product',
     initialState,
     reducers: {
-       setProduct:(state,action)=>{
-          state.productToFind=action.payload
-          
-       }
+        setProduct:(state,action)=>{
+            state.productToFind=action.payload
+            
+        }
     }
 })
+
 export const {setProduct} = productSlice.actions
 export default productSlice.reducer
+
+
